49b8eb46754fbcb6cdfdfcdfdbd02ea7e2816fa4
Gerben Droogers <gerben@droogers.eu>
2020-01-30 21:18:57 +0100

    Fix basic auth urls when downloading

--- src/plex.py
+++ src/plex.py
@@ -410,7 +410,16 @@ class Plex(GObject.Object):
         if not os.path.exists(path_dir):
             os.makedirs(path_dir)
         if not os.path.exists(path):
-            urllib.request.urlretrieve(url_image, path)
+            parse = urllib.parse.urlparse(url_image)
+            auth_user = parse.username
+            auth_passwd = parse.password
+            password_mgr = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+            password_mgr.add_password(None, parse.scheme + "://" + parse.hostname, auth_user, auth_passwd)
+            handler = urllib.request.HTTPBasicAuthHandler(password_mgr)
+            opener = urllib.request.build_opener(handler)
+            img_raw = opener.open(parse.scheme + "://" + parse.hostname + parse.path + "?" + parse.query)
+            with open(path, 'w+b') as file:
+                file.write(img_raw.read())
             return path
         else:
             return path
